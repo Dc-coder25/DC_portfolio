07d134c676b3f0e00ab20ca6f56f54b8b3de918c
DC <tolotrarandria25@gmail.com>
2026-03-08 08:15:13 +0300

    feat(navbar): add navigation bar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <div className="navbar bg-base-100">
+      <a className="btn btn-ghost text-xl">Mon Portfolio</a>
+      <ul className="menu menu-horizontal px-1">
+        <li><a>Projets</a></li>
+        <li><a>À propos</a></li>
+        <li><a>Contact</a></li>
+      </ul>
+    </div>
+  )
+}
